24d702a5dbbf4c7dc1d0cba01933eed1b1f15331
Tawashiiiii <imp.bonabona@gmail.com>
2023-03-12 18:28:57 +0900

    説明変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,11 +7,7 @@ defineProps<{
 <template>
   <div class="greetings">
     <h1 class="green">{{ msg }}</h1>
-    <h3>
-      You’ve successfully created a project with
-      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
-      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>. What's next?
-    </h3>
+    <h3>簡単なプロフィールと職務経歴を公開しています</h3>
   </div>
 </template>
 
